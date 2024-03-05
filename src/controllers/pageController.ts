1b98ad332150eef3cf5320db734281f1efecb60e
Diego Santos <diegofmsantos0@gmail.com>
2024-03-05 14:38:33 -0300

    projeto finalizado

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,55 @@
 import { Request, Response } from "express";
+import { createMenuObjetct } from '../helpers/createMenuObject'
+import { Pet } from '../models/pet'
 
 export const home = (req: Request, res: Response) => {
-    res.send('home no controller')
+    let list = Pet.getAll()
+
+    res.render('pages/page', {
+        menu: createMenuObjetct('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    })
 }
 
 export const dogs = (req: Request, res: Response) => {
-    
+    let list = Pet.getFromType('dog')
+
+    res.render('pages/page', {
+        menu: createMenuObjetct('dog'),
+        banner: {
+            title: 'Todos os cães',
+            background: 'banner_dog.jpg'
+        },
+        list
+    })
 }
 
 export const cats = (req: Request, res: Response) => {
-    
+    let list = Pet.getFromType('cat')
+
+    res.render('pages/page', {
+        menu: createMenuObjetct('cat'),
+        banner: {
+            title: 'Todos os gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    })
 }
 
 export const fishes = (req: Request, res: Response) => {
-    
+    let list = Pet.getFromType('fish')
+
+    res.render('pages/page', {
+        menu: createMenuObjetct('fish'),
+        banner: {
+            title: 'Todos os peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    })
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,7 @@ const router = Router()
 
 router.get('/', pageController.home)
 router.get('/dogs', pageController.dogs)
-router.get('cats/', pageController.cats)
+router.get('/cats', pageController.cats)
 router.get('/fishes', pageController.fishes)
 
 router.get('/search', searchController.search)
